e54da37503c4d662f60fde8d374db93c550a0f0d
v15hv4 <vishva2912@gmail.com>
2019-09-26 00:42:53 +0530

    Improve installscript and support updates

--- install.sh
+++ install.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
 INSTALL_TO=$HOME
 
-warn() {
-    echo "$1" >&2
-}
-
-die() {
-    warn "$1"
-    exit 1
-}
-
 install_vimrc () {
-    [ -e "$INSTALL_TO/vimrc" ] && die "$INSTALL_TO/vimrc already exists."
-    [ -e "~/.vim" ] && die "~/.vim already exists."
-    [ -e "~/.vimrc" ] && die "~/.vimrc already exists."
-
-    cd "$INSTALL_TO"
-    git clone git://github.com/v15hv4/vimrc.git wvimrc
-    cd wvimrc
-	mv vim ~/.vim
-    
-	# Symlink ~/.vimrc
 	cd ~
-    ln -s "$HOME/wvimrc/vimrc" .vimrc
-    
+	if [ -f ".vimrc" ]; then
+		if [ -f "old.vimrc" ]; then
+			rm -rf old.vimrc
+		fi
+		mv .vimrc old.vimrc	
+	fi	
+	if [ -d ".vim" ]; then
+		if [ -d "old.vim" ]; then
+			rm -rf old.vim
+		fi
+		mv .vim old.vim
+	fi
+
+	cd "$INSTALL_TO"
+    git clone git://github.com/v15hv4/vimrc.git wvimrc
+	mv wvimrc/vim ~/.vim
+    mv wvimrc/vimrc ~/.vimrc
+	rm -rf wvimrc
+
     echo "Done!"
 }
 
