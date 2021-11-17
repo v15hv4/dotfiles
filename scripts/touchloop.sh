ecca4b98af5d7a1f9182852037f5bd5f1829ed90
v15hv4 <vishva2912@gmail.com>
2021-11-17 18:54:25 +0530

    scripts: Add support for srun account flag in touchloop

--- scripts/touchloop.sh
+++ scripts/touchloop.sh
@@ -3,32 +3,35 @@
 PARTNAME=/scratch
 DIRNAME=$(whoami)
 NODELIST=()
+ACCOUNT=research
 
 # help text
 if [ $# -eq 0 ]
 then
-    echo "Usage: ./touchloop.sh -w <nodes> -p <partition> -d <directory name>" >&2
+    echo "Usage: ./touchloop.sh -w <nodes> -p <partition> -d <directory name> -A <account>" >&2
     exit
 fi
 
 # handle options
-while getopts ":w:p:d:" OPT
+while getopts ":w:p:d:A:" OPT
 do
     case $OPT in
-	w)
-	    NODELIST=("$OPTARG")
-	    until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [ -z $(eval "echo \${$OPTIND}") ]
-	    do
-		NODELIST+=($(eval "echo \${$OPTIND}"))
-		OPTIND=$((OPTIND + 1))
-	    done
-	    ;;
+        w)
+            NODELIST=("$OPTARG")
+            until [[ $(eval "echo \${$OPTIND}") =~ ^-.* ]] || [ -z $(eval "echo \${$OPTIND}") ]
+            do
+                NODELIST+=($(eval "echo \${$OPTIND}"))
+                OPTIND=$((OPTIND + 1))
+            done
+            ;;
         p)
             PARTNAME=$OPTARG
             ;;
         d)
             DIRNAME=$OPTARG
             ;;
+        A)
+            ACCOUNT=$OPTARG
     esac
 done
 
@@ -38,6 +41,6 @@ do
     node="gnode$idx"
 
     echo "Touching $PARTNAME/$DIRNAME in $node..."
-    srun -c 1 -w $node find $PARTNAME/$DIRNAME -exec touch {} +
+    srun -c 1 -A $ACCOUNT -w $node find $PARTNAME/$DIRNAME -exec touch {} +
     echo
 done
