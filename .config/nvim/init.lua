5364edded0ef19654d05260d0daa4c0cfc332779
v15hv4 <vishva2912@gmail.com>
2023-05-18 23:57:39 +0530

    Add nvim LSP and statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,13 +32,42 @@ require('packer').startup(function(use)
   use 'Th3Whit3Wolf/one-nvim'
 
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'nvim-tree/nvim-web-devicons'
 
   -- Git
   use 'lewis6991/gitsigns.nvim'
 
+  -- LSP
+  use { 
+    'VonHeikemen/lsp-zero.nvim', 
+    branch = 'v2.x', 
+    requires = {
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     -- Required
+      {'hrsh7th/cmp-nvim-lsp'}, -- Required
+      {'L3MON4D3/LuaSnip'},     -- Required
+    }
+  }
+
+  -- Statusline
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
   -- Other useful plugins
-  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+  use 'numToStr/Comment.nvim' -- 'gc' to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
   use 'm4xshen/autoclose.nvim' -- Automatically close brackets and quotes
 
@@ -85,8 +114,11 @@ vim.wo.number = true
 -- Show cursorline
 vim.wo.cursorline = true
 
+-- Don't show current mode
+vim.o.showmode = false
+
 -- Use Unicode
-vim.o.encoding = "utf-8"
+vim.o.encoding = 'utf-8'
 
 -- Tab configuration
 vim.o.tabstop = 4
@@ -121,12 +153,12 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
-vim.o.background = "light"
+vim.o.background = 'light'
 vim.o.termguicolors = true
 vim.cmd [[colorscheme one-nvim]]
 
 -- Fold on markers
-vim.o.foldmethod = "marker"
+vim.o.foldmethod = 'marker'
 
 -- Transparent background 
 vim.cmd [[hi Normal guibg=none]]
@@ -147,19 +179,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- KEYBINDS -- {{{
 -- Telescope
-local telescope_builtin = require("telescope.builtin")
-vim.keymap.set({"n", "i"}, "<C-b>", telescope_builtin.find_files, {})
-vim.keymap.set({"n", "i"}, "<C-f>", telescope_builtin.live_grep, {})
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set({'n', 'i'}, '<C-b>', ':Telescope file_browser<CR>', {})
+vim.keymap.set({'n', 'i'}, '<C-f>', telescope_builtin.live_grep, {})
 
 -- Navigating splits
-vim.keymap.set("n", "<C-Up>", "<C-W><C-Up>")
-vim.keymap.set("n", "<C-Down>", "<C-W><C-Down>")
-vim.keymap.set("n", "<C-Right>", "<C-W><C-L>")
-vim.keymap.set("n", "<C-Left>", "<C-W><C-H>")
+vim.keymap.set('n', '<C-Up>', '<C-W><C-Up>')
+vim.keymap.set('n', '<C-Down>', '<C-W><C-Down>')
+vim.keymap.set('n', '<C-Right>', '<C-W><C-L>')
+vim.keymap.set('n', '<C-Left>', '<C-W><C-H>')
 
 -- Navigating tabs
-vim.keymap.set("n", "<M-Right>", ":tabn<CR>")
-vim.keymap.set("n", "<M-Left>", ":tabp<CR>")
+vim.keymap.set('n', '<M-Right>', ':tabn<CR>')
+vim.keymap.set('n', '<M-Left>', ':tabp<CR>')
 -- }}}
 
 -- SPLITS -- {{{
@@ -168,7 +200,66 @@ vim.o.splitright = true
 -- }}}
 
 -- PLUGINS -- {{{
-require("Comment").setup()
-require("autoclose").setup()
+require('Comment').setup()
+require('autoclose').setup()
 require('gitsigns').setup()
+
+-- Telescope
+require('telescope').setup {
+  extensions = {
+    file_browser = {
+      hijack_netrw = true,
+    }
+  }
+}
+require('telescope').load_extension 'file_browser'
+
+-- LSP
+local lsp = require('lsp-zero').preset({})
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  }
+})
+
+-- Lualine
+local bubbles_onelight = require('lualine.themes.onelight')
+require('lualine').setup {
+  options = {
+    theme = bubbles_onelight,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'filename', 'branch' },
+    lualine_c = { 'fileformat' },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'progress' },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { 'location' },
+  },
+  tabline = {},
+  extensions = {},
+}
 -- }}}
